8230654bc0b84bb1cdad6ea0b027ab48be7b9383
atharva29 <atharvaajgaonkar29@gmail.com>
2025-03-01 18:19:13 +0000

    added circuit breaker to stripe

--- internal/psp/stripe/stripe.go
+++ internal/psp/stripe/stripe.go
@@ -5,7 +5,9 @@ import (
 	"os"
 	"payment-gateway/db"
 	"payment-gateway/internal/kafka"
+	"time"
 
+	"github.com/sony/gobreaker"
 	stripe "github.com/stripe/stripe-go/v81"
 )
 
@@ -14,6 +16,7 @@ type StripeClient struct {
 	secretKey string
 	accountID string
 	kafka     *kafka.Kafka
+	cb        *gobreaker.CircuitBreaker
 }
 
 // Init initializes the Stripe client with API key from environment
@@ -25,11 +28,25 @@ func Init(k *kafka.Kafka, db *db.DB) *StripeClient {
 		panic(fmt.Sprintf("missing Stripe secret key: %s or account ID: %s", secretKey, accountID))
 	}
 
+	cb := gobreaker.NewCircuitBreaker(gobreaker.Settings{
+		Name:        "StripePaymentIntent", // Name of the circuit breaker
+		MaxRequests: 2,                     // Number of requests allowed in half-open state
+		Interval:    60 * time.Second,      // Reset failure count every 60 seconds
+		Timeout:     30 * time.Second,      // Time to switch from open to half-open
+		ReadyToTrip: func(counts gobreaker.Counts) bool {
+			return counts.ConsecutiveFailures > 5 // Open circuit after 5 consecutive failures
+		},
+		OnStateChange: func(name string, from gobreaker.State, to gobreaker.State) {
+			fmt.Printf("Circuit Breaker '%s' changed from %s to %s\n", name, from, to)
+		},
+	})
+
 	stripe.Key = secretKey
 	client := &StripeClient{
 		secretKey: secretKey,
 		accountID: accountID,
 		kafka:     k,
+		cb:        cb,
 	}
 
 	go client.kafka.ConsumeStripeWebhook(client.GetTopic(), db, client.HandleWebhook)
